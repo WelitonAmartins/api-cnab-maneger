36bc6c743673a87337be21d8d72fb6ad0e913085
weliton.martins <weliton.martins@valemobi.com.br>
2024-05-11 00:29:20 -0300

    feat: processing in batch with spring batch

---
+++ src/main/java/br/com/wel/apicnabmaneger/ApiCnabManegerApplication.java
@@ -0,0 +1,13 @@
+package br.com.wel.apicnabmaneger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCnabManegerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCnabManegerApplication.class, args);
+	}
+
+}
